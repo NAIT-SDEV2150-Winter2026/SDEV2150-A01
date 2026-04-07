08bb52df9dc4bb0671e77f8367212759918655fc
Oliver Antoniu <oliver+github@abolish.golf>
2026-04-07 14:52:00 -0600

    add [09 - data fetching]: ex - add standalone API functions to be called by loaders

--- 09 - data fetching/example - data fetching/instructor/frontend/src/router.jsx
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/router.jsx
@@ -1,6 +1,17 @@
-// we'll just start off with some empty functions to bind to our react-router loaders
-// (they're not doing anything yet)
+import { getResources, getResourceById } from './api/resources';
 
-export async function resourceDirectoryLoader() {}
+export async function resourceDirectoryLoader() {
+  const resources = await getResources();
+  // whatever the named properties of the data items are -> what we'll be consuming
+  // by name in the components that use these loaders
+  return { resources };
+  /* 
+    usage in component: 
+      const data = useLoaderData();
+      console.log(data.resources);
+  */
+}
 
-export async function adminLoader() {}
+// this one's a little bit more complicated, because what we do depends on whether
+// we get a resourceId from the route (e.g. 'admin/' vs. 'admin/tutoring')
+export async function adminLoader({ params }) {}
